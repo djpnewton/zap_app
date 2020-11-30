e0bace465e960a020bc1bb1c4915bb5d64685bd0
Daniel Newton <djpnewton@gmail.com>
2020-11-30 16:24:02 +1300

    update firebase bundle id./ package name branding for frankie points use paydb change bundle/app id to me.zap.whitelabel.frankie

--- lib/config.dart
+++ lib/config.dart
@@ -10,17 +10,17 @@ enum TokenType { Waves, PayDB }
 // the default testnet value
 const TestnetDefault = false;
 // the app title
-const AppTitle = 'Zap Retailer';
+const AppTitle = 'Frankie';
 // all instances where the asset short name has the first letter capitalized
-const AssetShortName = 'Zap';
+const AssetShortName = 'FP';
 // all instances where the asset short name is lower case
-const AssetShortNameLower = 'zap';
+const AssetShortNameLower = 'fp';
 // all instances where the asset short name is upper case
-const AssetShortNameUpper = 'ZAP';
+const AssetShortNameUpper = 'FP';
 // the 'zap' icon on the header of the home page
 const AssetHeaderIconPng = 'assets/icon.png';
 // the lightning icon on the home page balance widget
-const AssetBalanceIcon = 'assets/icon-bolt.svg';
+const AssetBalanceIcon = 'assets/fp.svg';
 // enable/disable reward UI/feature
 const UseReward = true;
 // enable/disable settlement UI/feature
@@ -28,13 +28,13 @@ const UseSettlement = false;
 // enable/disable merchant api (including all references/conversions to NZD)
 const UseMerchantApi = false;
 // enable/disable a webview homepage (null = disabled)
-const String? WebviewURL = 'https://zap-me.github.io/zap-spa/index.html';
+const String? WebviewURL = null;
 // enable/disable the centered ZAP button
 const ZapButton = true;
 const ZapButtonIcon = FlutterIcons.bolt_faw5s;
 
 // are we using the waves network or a paydb server
-const TokenType AppTokenType = TokenType.Waves;
+const TokenType AppTokenType = TokenType.PayDB;
 
 // can we claim Redrat Zap tokens
 const ClaimRedRatZap =
@@ -52,7 +52,7 @@ const String? NodeUrlTestnet = null;
 
 // PayDB settings
 const String? PayDBServerMainnet = null;
-const String? PayDBServerTestnet = null;
+const String? PayDBServerTestnet = 'https://mtoken-test.zap.me/paydb/';
 
 // If set the app will try to register with the premio stage server for push notifications
 const String? PremioStageIndexUrl = null;
@@ -66,22 +66,19 @@ const String? StashServer = null;
 const String? CentrapayApiKey = 'PvfgoXxDv2FCzsgVsk1izM41e5tWd7C8AGuThG9M1';
 
 // PremioPay scheme and prefix (also needs to be changed in AndroidManifest.xml and Info.plist)
-const String PremioPayScheme = 'premiopay';
+const String PremioPayScheme = 'premiofrankiepoints';
 const String PremioPayPrefix = '$PremioPayScheme://';
 
 // PremioStageLink scheme and prefix (also needs to be changed in AndroidManifest.xml and Info.plist)
-const String PremioStageLinkScheme = 'premiostagelink';
+const String PremioStageLinkScheme = 'premiostagefrankiepoints';
 
 void initConfig() {
-  overrideTheme();
-  // example
-  /*overrideTheme(
-    zapWhite: Colors.lightBlue[50],
-    zapYellow: Colors.teal[100],
-    zapWarning: Colors.yellow,
-    zapWarningLight: Colors.yellow[100],
-    zapBlue: Colors.pink[200],
-    zapGreen: Colors.blueGrey[300],
-    zapTextThemer: GoogleFonts.sansitaTextTheme);
-    */
+  overrideTheme(
+      zapWhite: Colors.white,
+      zapYellow: Color.fromARGB(255, 218, 62, 82), //DA3E52 ~Amaranth
+      zapWarning: Colors.yellow,
+      zapWarningLight: Colors.yellow[100],
+      zapBlue: Color.fromARGB(255, 20, 89, 29), //14591D ~Lincoln Green
+      zapGreen: Color.fromARGB(255, 240, 162, 2), //F0A202 ~Marigold
+      zapTextThemer: GoogleFonts.robotoTextTheme);
 }
